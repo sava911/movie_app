261d30cdeaba9981c336409a0b425762a7a5b1a3
sava911 <pesss929@gmail.com>
2021-06-19 13:50:10 +0300

    'pprops'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+function Food({name}){
+
+  return(<h3>я люблю кушать {name}</h3>);
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> привет!!!!!!!!!</h1>
+      <Food name="борщ"/>
+      <Food name="суп"/>
+      <Food name="лук"/>
+      <Food name="перец"/>
+      <Food name="куку"/>
     </div>
   );
 }
